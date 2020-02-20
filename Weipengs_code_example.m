3abd82f5c03b9463995559b6d5338f213a57e4a8
linw5 <44678079+linw5@users.noreply.github.com>
2020-02-20 14:06:37 -0500

    Add files via upload

---
+++ Weipengs_code_example.m
@@ -0,0 +1,59 @@
+function arr = kevans_code_example;
+     %This reads in the input file, then filters by rfLength.
+     %arr is an array of 440s where each cell contains both the header
+     %  and the data as so {header, data}
+     [arr_440s, arr_200s, arr_52s] = get_all_records();
+     
+     %example of obstracting this data
+     fprintf('\nthis is a header (hdr)\n')
+     arr_440s(1).header
+     
+     fprintf('\nthese are the first 5 data points of index (record) 1\n')
+     all_data_points = arr_440s(1).data;
+     first_five = all_data_points(1:5);
+     fprintf('\t%g %g %g %g %g\n',first_five)
+     
+     %filter for meteors
+     meteors_200s = meteor_filter(arr_200s);
+     meteors_440s = meteor_filter(arr_440s);
+     meteors_52s = meteor_filter(arr_52s);
+     
+     %the same can be done for meteor arrays
+     meteor_stats=meteors_52s(1).meteor_stats
+     meteor_header=meteors_52s(1).header;
+     meteor_data=meteors_52s(1).data;
+     
+     %not confirmed just number of hits
+     fprintf('\nnumber of meteor detections per rfLen\n')
+     fprintf('\n\trfLen of 440 found %g meteors\n',size(meteors_440s,2))
+     fprintf('\trfLen of 200 found %g meteors\n',size(meteors_200s,2))
+     fprintf('\trfLen of 52 found %g meteors\n',size(meteors_52s,2))
+     
+     %lets abstract all meteor heights
+     %and times
+     c=1;
+     for i = 1:size(meteors_440s,2)
+         arr_heights(c)=meteors_440s(i).meteor_stats.ht;
+         arr_times(c)=meteors_440s(i).meteor_stats.time;
+         c = c+1;
+     end
+     for j = 1:size(meteors_200s,2)
+         arr_heights(c)=meteors_200s(j).meteor_stats.ht;
+         arr_times(c)=meteors_200s(j).meteor_stats.time;
+         c = c+1;
+     end
+     for z = 1:size(meteors_52s,2)
+         arr_heights(c)=meteors_52s(z).meteor_stats.ht;
+         arr_times(c)=meteors_52s(z).meteor_stats.time;
+         c = c+1;
+     end
+     arr_temp= [ arr_heights; arr_times ];
+     %sort by time
+     arr = sortrows(arr_temp,2);
+     figure(1)
+     scatter(arr(1,:),arr(2,:))
+     title('Time vs Meteor Heights')
+     xlabel('time')
+     ylabel('height (km)')
+     
+end
