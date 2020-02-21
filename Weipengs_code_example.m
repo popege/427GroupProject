af6f852e8a41f030ae0e00acf3cd2e23b0b2e619
linw5 <44678079+linw5@users.noreply.github.com>
2020-02-20 19:02:28 -0500

    Update Weipengs_code_example.m

--- Weipengs_code_example.m
+++ Weipengs_code_example.m
@@ -1,4 +1,4 @@
-function arr = kevans_code_example;
+function arr1 = kevans_code_example;
      %This reads in the input file, then filters by rfLength.
      %arr is an array of 440s where each cell contains both the header
      %  and the data as so {header, data}
@@ -32,26 +32,39 @@ function arr = kevans_code_example;
      %lets abstract all meteor heights
      %and times
      c=1;
+     a=1;
+     b=1;
      for i = 1:size(meteors_440s,2)
-         arr_heights(c)=meteors_440s(i).meteor_stats.ht;
-         arr_times(c)=meteors_440s(i).meteor_stats.time;
-         c = c+1;
+         arr_heights440s(a)=meteors_440s(i).meteor_stats.ht;
+         arr_times440s(a)=meteors_440s(i).meteor_stats.time;
+         a = a+1;
      end
      for j = 1:size(meteors_200s,2)
-         arr_heights(c)=meteors_200s(j).meteor_stats.ht;
-         arr_times(c)=meteors_200s(j).meteor_stats.time;
-         c = c+1;
+         arr_heights200s(b)=meteors_200s(j).meteor_stats.ht;
+         arr_times200s(b)=meteors_200s(j).meteor_stats.time;
+         b = b+1;
      end
      for z = 1:size(meteors_52s,2)
-         arr_heights(c)=meteors_52s(z).meteor_stats.ht;
-         arr_times(c)=meteors_52s(z).meteor_stats.time;
+         arr_heights52s(c)=meteors_52s(z).meteor_stats.ht;
+         arr_times52s(c)=meteors_52s(z).meteor_stats.time;
          c = c+1;
      end
-     arr_temp= [ arr_heights; arr_times ];
+     arr_temp1= [ arr_heights440s; arr_times440s ];
+     arr_temp2= [ arr_heights200s; arr_times200s ];
+     arr_temp3= [ arr_heights52s; arr_times52s ];
+     
      %sort by time
-     arr = sortrows(arr_temp,2);
+     arr1 = sortrows(arr_temp1,2);
+     arr2 = sortrows(arr_temp2,2);
+     arr3 = sortrows(arr_temp3,2);
      figure(1)
-     scatter(arr(1,:),arr(2,:))
+     scatter(arr1(1,:),arr1(2,:))
+     hold on;
+     scatter(arr2(1,:),arr2(2,:))
+     hold on;
+     scatter(arr3(1,:),arr3(2,:))
+     legend('meteors_440s','meteors_200s','meteors_52s');
+     hold off;
      title('Time vs Meteor Heights')
      xlabel('time')
      ylabel('height (km)')
